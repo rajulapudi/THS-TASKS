d15264ca59337237e4a440014874ae38803cdee3
praneeth <rajulapudip@gmail.com>
2019-05-23 23:11:30 +0530

    algo update

---
+++ week1tasks/algotasks.js
@@ -0,0 +1,24 @@
+/*to subtract without using minus sign*/
+
+/*var a = 89;
+var b = 7;
+
+let c = ~b;
+let subwithoutminus =a+c+1;
+
+console.log("the result is" + subwithoutminus);*/
+
+
+/*to find remainder without mod function*/
+
+/* max remainder can be a-1*/
+
+let a = 44;/*enter a small num*/
+let b = 98;/*enter a larger num*/
+
+let c = b-a;
+
+for (x=c; x<=b; x++){
+    if (x/a == 0){
+        console.log("result is " + b-x);
+    }}
