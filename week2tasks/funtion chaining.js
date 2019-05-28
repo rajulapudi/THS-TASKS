97ddf271bb26eb2d9786e887df926999c218abb5
praneeth <rajulapudip@gmail.com>
2019-05-28 15:47:14 +0530

    functionchaining

--- week2tasks/funtion chaining.js
+++ week2tasks/funtion chaining.js
@@ -1,3 +1,24 @@
-function a()={
-    b:function b(){console.log("Hello");}
-    };
+function a() {
+    return {
+        b: function b() {
+            return {
+                c: function c() {
+                    console.log("Hello")
+                }
+            }
+        }
+    }
+}
+
+a().b().c() will return hello - here . (dot) notation indicates that we are working with objects
+
+function a() {
+    return {
+        b: {c: function c(){console.log("hello")}}}}
+
+
+
+
+var a = "abc"
+var b = "cde"
+
