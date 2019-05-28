40e4dffe1f9de73b074493b5ee5d88853de6578c
praneeth <rajulapudip@gmail.com>
2019-05-28 21:03:34 +0530

    batterylogic

--- week2tasks/Battery App/index.js
+++ week2tasks/Battery App/index.js
@@ -8,9 +8,46 @@ window.onload = function() {
                 var batlevel = battery.level * 100;
 
                 document.querySelector('#dischargingTime').textContent = battery.dischargingTime / 3600 + " hrs";
-                if (battery.level >= 0.5){
-                    document.getElementById("bar0").style.background="yellow";
-                    console.log("gotit")
+                if (battery.level >= 0.81){
+                    document.getElementById("bar0").style.background="green";
+                    document.getElementById("bar1").style.background="green";
+                    document.getElementById("bar2").style.background="green";
+                    document.getElementById("bar3").style.background="green";
+                    document.getElementById("bar4").style.background="green";
+                    document.getElementById("bar5").style.background="green";
+                    
+                }else if (battery.level >= 0.61){
+                    document.getElementById("bar0").style.background="white";
+                    document.getElementById("bar1").style.background="green";
+                    document.getElementById("bar2").style.background="green";
+                    document.getElementById("bar3").style.background="green";
+                    document.getElementById("bar4").style.background="green";
+                    document.getElementById("bar5").style.background="green";
+                    
+                }else if (battery.level >= 0.41){
+                    document.getElementById("bar0").style.background="white";
+                    document.getElementById("bar1").style.background="white";
+                    document.getElementById("bar2").style.background="yellow";
+                    document.getElementById("bar3").style.background="yellow";
+                    document.getElementById("bar4").style.background="yellow";
+                    document.getElementById("bar5").style.background="yellow";
+                    
+                }else if (battery.level >= 0.21){
+                    document.getElementById("bar0").style.background="white";
+                    document.getElementById("bar1").style.background="white";
+                    document.getElementById("bar2").style.background="white";
+                    document.getElementById("bar3").style.background="white";
+                    document.getElementById("bar4").style.background="yellow";
+                    document.getElementById("bar5").style.background="yellow";
+                    
+                }else if (battery.level >= 0.11){
+                    document.getElementById("bar0").style.background="white";
+                    document.getElementById("bar1").style.background="white";
+                    document.getElementById("bar2").style.background="white";
+                    document.getElementById("bar3").style.background="white";
+                    document.getElementById("bar4").style.background="white";
+                    document.getElementById("bar5").style.background="red";
+                    
                 }
             }
 
