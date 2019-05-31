4ae02d77a22294a50e723dcc6381e289b2198a1b
praneeth <rajulapudip@gmail.com>
2019-05-31 17:42:52 +0530

    expressjs and much more

--- week2tasks/npmnode/index.js
+++ week2tasks/npmnode/index.js
@@ -1,3 +1,28 @@
-var readline = require("readline-sync");
+var express = require("express");
+var port = 8080;
+const app = express();
+/*
+var server = http.createServer((req,res)=>{
+    res.end("Hello"+"\n")
+});
 
-var 
+server.listen(port, ()=>{
+    console.log("Hi this is from server");
+});
+*/
+
+
+
+
+app.get("/",(req,res)=>{
+    res.send("This is 1st Message");//we can only send one response
+});
+
+app.get("/login", (req,res)=>{
+    res.send("I am login");
+});
+
+app.listen(port,()=>{console.log("Server started at :" + port)});
+
+
+
